523abbc40ea2a06ad11c0ef065986169991a308f
jcusc <20010177@usc.edu.ph>
2026-09-17 09:30:43 +0800

    le12
    - made the code
    - but no gui

--- src/le12/le12.java
+++ src/le12/le12.java
@@ -1,10 +1,26 @@
 package le12;
+import java.util.Scanner;
 
 public class le12 {
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter a number between 0 and 1000: ");
+	        int number = scanner.nextInt();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	        int originalNumber = number;
+	        int product = 1;
 
-	}
-
+	        if (number == 0) {
+	            product = 0;
+	        } else {
+	            while (number > 0) {
+	                int digit = number % 10;
+	                product *= digit;
+	                number /= 10;
+	                               }
+	               }
+	        System.out.println("The product of all digits in " + originalNumber + " is " + product);
+	        scanner.close();
+	    }
 }
+
